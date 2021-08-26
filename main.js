ebd2a55a4e376a0da1dd85e5f19d49c31b317a7c
arnavkh21 <72095856+arnavkh21@users.noreply.github.com>
2021-08-26 12:22:02 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 img = "";
 status = "";
+objects = [];
 function preload(){
     img = loadImage('dog_cat.jpg');
 }
@@ -11,17 +12,176 @@ function setup(){
 }
 function draw(){
     image(img ,0,0,640,420);
-    fill("#000000");
-    text("Dog",45,75);
-    noFill();
-    stroke("#000000");
-    rect(30,60,450,350);
+   
+     
+  if(status != ""){
+     for(i = 0; i < objects.length; i++){
+         document.getElementById("status").innerHTML = "Status : Object Detected";
+         fill("#000000");
+         percent = floor(objects[i].confidence * 100);
+         text(objects[i].label + "" + percent + "%", objects[i].x, objects[i].y);
+         noFill();
+         stroke("#000000");
+         rect(objects[i].x,  objects[i].y, objects[i].width, objects[i].height);
 
-    fill("#000000");
-    text("cat",320,120);
-    noFill();
-    stroke("#000000");
-    rect(300,90,270,320);
+     } 
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
 }
 
 
@@ -512,9 +672,10 @@ function draw(){
 
 
  
- function gotResult(){
+ function gotResult(error,results){
      if(error){
      console.error(error);
      }
      console.log(results);
+     objects = results;
  }
